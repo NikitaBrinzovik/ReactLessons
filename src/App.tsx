7476879f4fb0156fb8947477904d1dd9b799cfc0
Nikita <knanaprimer@gmail.com>
2021-04-09 15:53:22 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from './components1/Accordion/accordion';
+import Rating from './components1/Rating';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageTitle title={"This is App title"}/>
+      {/*<Header />*/}
+      <PageTitle title={"My friends"}/>
+      Article 1
+      <Rating value={3}/>
+      <Accordion title={"Menu"} collapsed={false}/>
+      Article 2
+      <Rating value={5}/>
+      <Rating value={5}/>
+      <Rating value={3}/>
+      <Rating value={2}/>
+      <Accordion title={"second menu"} collapsed={true}/>
     </div>
   );
 }
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
